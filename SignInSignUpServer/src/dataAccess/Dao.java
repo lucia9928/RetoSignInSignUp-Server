e826f66466d741e02ac7ce406159875d01dec879
2dam <2dam@HZ395680.TartangaLH.eus>
2024-10-29 11:42:19 +0100

    dao con login 2.

--- SignInSignUpServer/src/dataAccess/Dao.java
+++ SignInSignUpServer/src/dataAccess/Dao.java
@@ -5,7 +5,6 @@
  */
 package dataAccess;
 
-import excepciones.CreateException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -39,28 +38,26 @@ public class Dao implements Signable {
         int idGenerado = 0;
 
         try {
-            
+
             System.out.println("Conexion exitosa a la base de datos de odoo");
 
             String sql = "insert into res_partner(name, street, zip, city, email, phone) values(?, ?, ?, ?, ?, ?)";
             preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, user.getNombre()+"-"+user.getApellido());
+            preparedStatement.setString(1, user.getNombre() + "-" + user.getApellido());
             preparedStatement.setString(2, user.getCalle());
-            preparedStatement.setString(3, user.getCodigoPostal());
+            preparedStatement.setString(3, user.getCodPostal());
             preparedStatement.setString(4, user.getCiudad());
             preparedStatement.setString(5, user.getEmail());
-            preparedStatement.setInt(6, user.getTelefono());
+            preparedStatement.setString(6, user.getTelefono());
             preparedStatement.executeUpdate();
-            
-            
-            sql="select id from res_partner order by id desc limit 1";
-            preparedStatement=connection.prepareStatement(sql);
-            resultSet=preparedStatement.executeQuery();
-            if(resultSet.next()){
-                idGenerado=resultSet.getInt(1);
+
+            sql = "select id from res_partner order by id desc limit 1";
+            preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                idGenerado = resultSet.getInt(1);
             }
-            
-            
+
             sql = "insert into res_users (company_id, partner_id, active, login, password, notification_type) values (?, ?, ?, ?, ?,?)";
             preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, 1);
@@ -70,9 +67,7 @@ public class Dao implements Signable {
             preparedStatement.setString(5, user.getContrasena());
             preparedStatement.setString(6, "email");
             preparedStatement.executeUpdate();
-            
-           
-           
+
             conexion.devolverConexion(connection);
         } catch (SQLException ex) {
             Logger.getLogger(Dao.class.getName()).log(Level.SEVERE, null, ex);
@@ -97,7 +92,38 @@ public class Dao implements Signable {
 
     @Override
     public Usuario login(Usuario user) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = conexion.obtenerConexion();
+
+            String sql = "SELECT login, password FROM res_users WHERE login = ? AND password = ?;";
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, user.getEmail());
+            preparedStatement.setString(2, user.getContrasena());
+
+            resultSet = preparedStatement.executeQuery();
+
+        } catch (SQLException e) {
+            Logger.getLogger(Dao.class.getName()).log(Level.SEVERE, "Error al autenticar el usuario", e);
+            throw e;
+
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+
+        return user;
     }
 
 }
